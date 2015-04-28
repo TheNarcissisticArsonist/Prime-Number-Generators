a3060e5b85afc032384a43344eca4476833b0c0b
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-04-28 17:40:32 -0400

    Code works

--- HTML-Javascript/Generate.js
+++ HTML-Javascript/Generate.js
@@ -1,3 +1,34 @@
 primes = [2, 3, 5, 7];
 div = document.getElementById("primes");
-div.innerHTML = primes;
+div.innerHTML = primes.join(", ");
+
+next = document.getElementById("next");
+next.addEventListener("click", nextPrime);
+
+document.addEventListener("keydown", function(event) {
+  console.log(event.which);
+  if(event.which = 13) {
+    nextPrime();
+  }
+});
+
+
+function nextPrime() {
+  currentNum = primes[primes.length - 1];
+  foundNext = false;
+  while(!foundNext) {
+    currentNum++;
+    broke = false;
+    for(i=0; i<primes.length; i++) {
+      if(currentNum % primes[i] == 0) {
+        broke = true;
+        break;
+      }
+    }
+    if(!broke) {
+      foundNext = true;
+      primes.push(currentNum);
+    }
+  }
+  div.innerHTML = primes.join(", ");
+}
