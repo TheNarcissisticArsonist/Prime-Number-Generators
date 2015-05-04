3fb12b265678458fc36b2a4a3dcd38dd1007c994
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-05-03 21:19:07 -0400

    Added buttons for 10, 100, 1000 etc. up to 100000

--- HTML-Javascript/Generate.js
+++ HTML-Javascript/Generate.js
@@ -5,6 +5,41 @@ div.innerHTML = primes.join(", ");
 next = document.getElementById("next");
 next.addEventListener("click", nextPrime);
 
+next10 = document.getElementById("next10");
+next10.addEventListener("click", function() {
+  for(j=0; j<10; j++) {
+    nextPrime();
+  }
+});
+
+next100 = document.getElementById("next100");
+next100.addEventListener("click", function() {
+  for(j=0; j<100; j++) {
+    nextPrime();
+  }
+});
+
+next1000 = document.getElementById("next1000");
+next1000.addEventListener("click", function() {
+  for(j=0; j<1000; j++) {
+    nextPrime();
+  }
+});
+
+next10000 = document.getElementById("next10000");
+next10000.addEventListener("click", function() {
+  for(j=0; j<10000; j++) {
+    nextPrime();
+  }
+});
+
+next100000 = document.getElementById("next100000");
+next100000.addEventListener("click", function() {
+  for(j=0; j<100000; j++) {
+    nextPrime();
+  }
+});
+
 document.addEventListener("keydown", function(event) {
   console.log(event.which);
   if(event.which == 13) {
